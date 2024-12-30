08163d1183d95e29907e57c25052cd5f2f1f3617
unknown <designwithtawhid@gmail.com>
2024-12-30 23:57:31 +0600

    firebase set successfully

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,14 +1,12 @@
-
+import Lottie from "lottie-react";
+import registerAnimationData from "../../assets/register.json"
 
 const Register = () => {
     return (
         <div className="hero bg-base-200 min-h-screen">
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <div className="text-center lg:text-left">
-                    <p className="py-6">
-                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                        quasi. In deleniti eaque aut repudiandae et a id nisi.
-                    </p>
+                    <Lottie animationData={registerAnimationData}></Lottie>
                 </div>
                 <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
                 <h1 className="mt-5 pl-10 text-4xl font-bold">Register Now!</h1>
