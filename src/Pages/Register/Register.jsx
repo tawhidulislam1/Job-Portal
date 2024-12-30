9c9ee90c29c73e0421fcb823f802ffd47fffc484
unknown <designwithtawhid@gmail.com>
2024-12-31 00:25:07 +0600

    register page added

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,7 +1,24 @@
 import Lottie from "lottie-react";
 import registerAnimationData from "../../assets/register.json"
+import { useContext } from "react";
+import AuthContext from "../../Context/AuthContext";
 
 const Register = () => {
+    const {createUser} = useContext(AuthContext)
+    const handleRegister = e => {
+        e.preventDefault()
+        const form = e.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+        createUser(email,password)
+        .then(result =>{
+            console.log(result);
+        })
+        .catch(error =>{
+            console.log(error);
+        })
+    }
     return (
         <div className="hero bg-base-200 min-h-screen">
             <div className="hero-content flex-col lg:flex-row-reverse">
@@ -9,20 +26,20 @@ const Register = () => {
                     <Lottie animationData={registerAnimationData}></Lottie>
                 </div>
                 <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-                <h1 className="mt-5 pl-10 text-4xl font-bold">Register Now!</h1>
+                    <h1 className="mt-5 pl-10 text-4xl font-bold">Register Now!</h1>
 
-                    <form className="card-body">
+                    <form className="card-body" onSubmit={handleRegister}>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input type="email" placeholder="email" className="input input-bordered" required />
+                            <input type="email" name="email" placeholder="email" className="input input-bordered" required />
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="password" placeholder="password" className="input input-bordered" required />
+                            <input type="password" name="password" placeholder="password" className="input input-bordered" required />
                             <label className="label">
                                 <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                             </label>
--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,11 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import router from './router/router.jsx';
+import AuthProvider from './Context/AuthProvider.jsx';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <AuthProvider>
       <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>,
 )
