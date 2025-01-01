390c7ce13feecc8026c49c7462a4567928f31812
unknown <designwithtawhid@gmail.com>
2025-01-02 02:32:26 +0600

    updated job apply page

--- src/Pages/JobApply/JobApply.jsx
+++ src/Pages/JobApply/JobApply.jsx
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom";
 import useAuth from "../../Hook/useAuth";
+import Swal from "sweetalert2";
 
 const JobApply = () => {
     const { id } = useParams();
@@ -15,44 +16,55 @@ const JobApply = () => {
             application_email: user.email,
             linkdin, github, resume
         }
-        console.log(jobApplication);
+        fetch('http://localhost:5000/job-application', {
+            method: "POST",
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(jobApplication)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.insertedId) {
+                    Swal.fire({
+                        position: "top-center",
+                        icon: "success",
+                        title: "Your work has been saved",
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                }
+            })
     }
     return (
-        <div className="hero bg-base-200 min-h-screen">
-            <div className="hero-content flex-col lg:flex-row-reverse">
-                <div className="text-center lg:text-left">
-                    <h1 className="text-5xl font-bold">Login now!</h1>
-                    <p className="py-6">
-                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                        quasi. In deleniti eaque aut repudiandae et a id nisi.
-                    </p>
-                </div>
-                <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-                    <form onSubmit={handleSubmit} className="card-body">
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Linkdin URL</span>
-                            </label>
-                            <input type="url" name="linkdin" placeholder="Linkdin URL" className="input input-bordered" required />
-                        </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Github Url</span>
-                            </label>
-                            <input type="url" name="github" placeholder="Github Url" className="input input-bordered" required />
-                        </div>
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text">Resume Url</span>
-                            </label>
-                            <input type="url" name="resume" placeholder="Github Url" className="input input-bordered" required />
-                        </div>
-                        <div className="form-control mt-6">
-                            <button className="btn btn-primary">Login</button>
-                        </div>
-                    </form>
-                </div>
+        <div className="card bg-base-100 w-full my-10 shadow-2xl">
+            <div className="text-center ">
+                <h1 className="text-5xl font-bold">Good Luck For Apply</h1>
             </div>
+            <form onSubmit={handleSubmit} className="card-body">
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Linkdin URL</span>
+                    </label>
+                    <input type="url" name="linkdin" placeholder="Linkdin URL" className="input input-bordered" required />
+                </div>
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Github Url</span>
+                    </label>
+                    <input type="url" name="github" placeholder="Github Url" className="input input-bordered" required />
+                </div>
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Resume Url</span>
+                    </label>
+                    <input type="url" name="resume" placeholder="Github Url" className="input input-bordered" required />
+                </div>
+                <div className="form-control mt-6">
+                    <button className="btn btn-primary">Login</button>
+                </div>
+            </form>
         </div>
     );
 };
